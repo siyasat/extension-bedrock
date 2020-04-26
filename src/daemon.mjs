228bb2f7e00a89fda3cdc764a07fd33e89a71001
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-04-26 22:27:33 +0800

    feat(src): add daemon.mjs

---
+++ src/daemon.mjs
@@ -0,0 +1,61 @@
+// Import Modules
+import { Browser } from './libs/browser.js';
+import { Siyasat } from './libs/siyasat.js';
+
+// Get Browser object
+const browser = Browser.get();
+
+// Get Browser Runtime
+const runtime = Browser.getRuntime();
+
+/**
+ * onInstall
+ * Tasks to do on install
+ */
+runtime.onInstalled.addListener(() => {
+    Browser.store.get('done_onboard').then(res => {
+        if(!res) {
+            Browser.tab.create({ url: 'https://my.siyasat.org' });
+            Browser.store.set({ done_onboard: true });
+        }
+    });
+});
+
+/**
+ * onStartup
+ * Tasks to do on startup
+ */
+runtime.onStartup.addListener(()=> {
+    // List all context menu items
+    const starterContextMenuArray = [
+        {
+            id: "report_link",
+            title: "Report Link",
+            contexts: ["link"]
+        },
+        {
+            id: "fact_check",
+            title: "Fact Check",
+            contexts: ["selection"]
+        }
+    ];
+    // Add context menu items
+    Browser.contextMenu.create(starterContextMenuArray);
+});
+
+/**
+ * onMessage
+ * Handle messages sent by extension and content scripts
+ */
+runtime.onMessage.addListener(object => {
+
+});
+
+/**
+ * Tab onUpdated
+ * Handle events emitted by tabs
+ */
+browser.tabs.onUpdated.addListener((tab_id, change_info, tab) => {
+    // Handle URL Load
+    if(change_info.status === 'loading' && change_info.url) Siyasat.handleUrlLoad(tab_id, change_info, tab);
+});
